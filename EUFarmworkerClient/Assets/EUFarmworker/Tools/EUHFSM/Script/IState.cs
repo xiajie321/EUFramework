a18ae01b15445bfb781552394bbcb569fd1b947d
XiaJie <3113959631@qq.com>
2026-01-04 01:28:35 +0800

    [新增] 分层状态机

---
+++ EUFarmworkerClient/Assets/EUFarmworker/Tools/EUHFSM/Script/IState.cs
@@ -0,0 +1,10 @@
+namespace EUFarmworker.Tools.EUFSM
+{
+    public interface IState
+    {
+        void OnEnter();
+        void OnUpdate();
+        void OnFixedUpdate();
+        void OnExit();
+    }
+}
