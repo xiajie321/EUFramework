ee81d01f3785d97596dbc0fd304d0e65ba382c88
XiaJie <3113959631@qq.com>
2026-01-04 22:19:24 +0800

    [新增] EUAudio工具(未完工)

---
+++ EUFarmworkerClient/Assets/EUFarmworker/Tools/EUAudio/Script/EUAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using EUFarmworker.Core.Abstracts;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace EUFarmworker.Tools.EUAudio.Script
+{
+    public class EUAudio : AbstractUtility
+    {
+        private EUAudioManagerMono _root;
+
+        public override void Init()
+        {
+            _root = new GameObject("EUAudio").AddComponent<EUAudioManagerMono>();
+            Object.DontDestroyOnLoad(_root.gameObject);
+        }
+    }
+}
