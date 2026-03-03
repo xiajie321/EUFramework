8044993f018a6439b3523d2929a1e1a1092d6f76
XiaJie <3113959631@qq.com>
2026-03-03 16:07:55 +0800

    [更改] 增加事件注册与注销的安全性

--- EUFramworkClient/Assets/EUFramework/Core/MVC/CoreTool/TypeEventSystem.cs
+++ EUFramworkClient/Assets/EUFramework/Core/MVC/CoreTool/TypeEventSystem.cs
@@ -72,6 +72,7 @@ namespace EUFramework.Core.MVC.CoreTool
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Register<T>(Action<T> handler) where T : struct
         {
+            if(handler == null) return;
             // 追踪类型以便Clear时清理（仅首次注册时执行）
             if (!EventCache<T>.IsTracked)
             {
@@ -103,6 +104,7 @@ namespace EUFramework.Core.MVC.CoreTool
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void UnRegister<T>(Action<T> handler) where T : struct
         {
+            if(handler == null) return;
             var handlers = EventCache<T>.Handlers;
             ref var count = ref EventCache<T>.Count;
             
